c07bdbc1731d1e64c4dee253cf47b63b0d532abc
Raku Zeta <zetaraku@gmail.com>
2023-05-23 20:52:53 +0800

    ✨ use fastify.httpErrors method from @fastify/sensible plugin

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,10 @@ const app: FastifyPluginAsync = async (fastify, options) => {
   fastify.get('/', async (request, reply) => {
     return { message: 'Hello world!' };
   });
+
+  fastify.get('/418', async (request, reply) => {
+    throw fastify.httpErrors.imateapot();
+  });
 };
 
 export default app;
