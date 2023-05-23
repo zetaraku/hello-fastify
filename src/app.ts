b27b4c7098aa46dd3e09abe5c0a241572e32d4ce
Raku Zeta <zetaraku@gmail.com>
2023-05-23 20:52:18 +0800

    ✨ register @fastify/sensible plugin

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import { FastifyPluginAsync } from 'fastify';
+import fastifySensiblePlugin from '@fastify/sensible';
 
 const app: FastifyPluginAsync = async (fastify, options) => {
+  await fastify.register(fastifySensiblePlugin);
+
   fastify.get('/', async (request, reply) => {
     return { message: 'Hello world!' };
   });
