10963a5c2faa2096793fc1cb9a3f0097d5417588
Raku Zeta <zetaraku@gmail.com>
2023-05-23 21:27:42 +0800

    ✨ add GET /greet route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,19 @@
 import { FastifyPluginAsync } from 'fastify';
+import { z } from 'zod';
 
 const routes: FastifyPluginAsync = async (fastify, options) => {
   fastify.get('/', async (request, reply) => {
     return { message: 'Hello world!' };
+  });
+
+  fastify.get('/greet', {
+    schema: {
+      querystring: z.object({
+        name: z.string(),
+      }),
+    },
+  }, async (request, reply) => {
+    return { message: `Hi ${request.query.name}!` };
   });
 
   fastify.get('/418', async (request, reply) => {
