dd1662d3069b569f988aa7f818a18acf63538c67
Raku Zeta <zetaraku@gmail.com>
2023-05-23 21:33:16 +0800

    ♻️ use @fastify/autoload to load routes

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import * as path from 'node:path';
 import { FastifyPluginAsync } from 'fastify';
 import autoloadPlugin from '@fastify/autoload';
-import routesPlugin from './routes';
 
 const app: FastifyPluginAsync = async (fastify, options) => {
   await fastify.register(autoloadPlugin, {
     dir: path.join(__dirname, 'plugins'),
   });
 
-  await fastify.register(routesPlugin);
+  await fastify.register(autoloadPlugin, {
+    dir: path.join(__dirname, 'routes'),
+    dirNameRoutePrefix: true,
+    routeParams: true,
+  });
 };
 
 export default app;
