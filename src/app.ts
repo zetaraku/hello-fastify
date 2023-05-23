bc6ed50ba19d499318fec31313053aa93408f909
Raku Zeta <zetaraku@gmail.com>
2023-05-23 21:24:32 +0800

    ✨ use zod plugin

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import { FastifyPluginAsync } from 'fastify';
 import sensiblePlugin from './plugins/sensible';
+import zodPlugin from './plugins/zod';
 import routesPlugin from './routes';
 
 const app: FastifyPluginAsync = async (fastify, options) => {
   await fastify.register(sensiblePlugin);
+  await fastify.register(zodPlugin);
 
   await fastify.register(routesPlugin);
 };
