3d54f21414728b40f65b0b08d932cded4fa7f5db
Raku Zeta <zetaraku@gmail.com>
2023-05-30 19:49:44 +0800

    ✨ add GET /ping route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,4 @@
+import * as child_process from 'node:child_process';
 import { FastifyPluginAsync } from 'fastify';
 import * as z from 'zod';
 
@@ -5,6 +6,13 @@ const routes: FastifyPluginAsync = async (fastify, options) => {
   fastify.get('/', {
   }, async (request, reply) => {
     return { message: 'Hello world!' };
+  });
+
+  fastify.get('/ping', {
+  }, async (request, reply) => {
+    const child = child_process.spawn('ping', ['127.0.0.1', '-t']);
+
+    await reply.type('text/plain; charset=utf-8').send(child.stdout);
   });
 
   fastify.get('/greet', {
